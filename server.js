3d557c24a250cebed5b12d5561b7aacdef8a1d84
Takio W <takiyono@outlook.com>
2018-08-12 15:03:01 -0400

    angular added

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+var bodyParser = require("body-parser");
+
+const app = express();
+
+
+app.use(express.static(__dirname + '/dist'));
+app.use(bodyParser.json());
+
+app.get('/*', function(req,res) {
+
+  res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
+
+
+app.listen(process.env.PORT || 8080);
